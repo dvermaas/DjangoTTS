583f9727c1c847d85f677fdf04e58d4362e274e0
Dacruon <dvermaas2@gmail.com>
2023-02-13 21:32:33 +0100

    login

--- DjangoTTS/urls.py
+++ DjangoTTS/urls.py
@@ -18,5 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("polls.urls"), name="home"),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("polls/", include("polls.urls")),
+    path("members/", include("django.contrib.auth.urls")),
+    path("members/", include("members.urls")),
 ]
